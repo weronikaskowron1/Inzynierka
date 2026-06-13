9dc1f73df701805e95f183948bec03b5b35aa599
Weronika <weronikaskowron1@gmail.com>
2026-06-13 21:46:34 +0200

    szablon nawigacji

--- App.tsx
+++ App.tsx
@@ -1,12 +1,70 @@
+import * as React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import { useNavigation, NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import { Button } from '@react-navigation/elements';
+
+function HomeScreen() {
+  const navigation = useNavigation();
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+      <Button onPress={() => navigation.navigate('Details')}>
+        Go to Details
+      </Button>
+
+    </View>
+  );
+}
+
+function DetailsScreen() {
+  const navigation = useNavigation();
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>DetailsScreen</Text>
+      <Button onPress={() => navigation.goBack()}>Go back</Button>
+    </View>
+  );
+}
+
+const Stack = createNativeStackNavigator();
+
+function RootStack() {
+  return (
+
+    <Stack.Navigator
+    // domyślna strona
+        initialRouteName="Home"
+    // opcje dla wszystkich zakładek
+        screenOptions={{
+             headerStyle: { backgroundColor: 'tomato' },
+        }}
+    >
+        <Stack.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{ title: 'Home' }}
+        />
+
+        <Stack.Screen
+        name="Details"
+        component={DetailsScreen}
+        options={{ title: 'Details' }}
+        />
+     </Stack.Navigator>
+  );
+}
+
+
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+        <RootStack />
+    </NavigationContainer>
+
   );
 }
 
