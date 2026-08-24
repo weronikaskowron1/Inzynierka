d95aa5dd740b694f47074ed950becbb7a3c2b6f6
Weronika <weronikaskowron1@gmail.com>
2026-08-24 02:20:31 +0200

    togel

--- Frontend/Komponenty/Rezerwacje/CalendarToggle.tsx
+++ Frontend/Komponenty/Rezerwacje/CalendarToggle.tsx
@@ -1,12 +1,38 @@
 import { Text, View, StyleSheet, Pressable, ScrollView } from "react-native";
+import { useState } from "react";
+
 import { Colors } from "../../Themes/colors.ts";
 
 const CalendarToggle = () => {
+  const [selectedMonthCalendar, setSelectedMonthCalendar] = useState(false);
+  const [selectedWeekCalendar, setSelectedWeekCalendar] = useState(true);
   return (
     <View style={styles.container}>
       <View style={styles.toggle_container}>
-        <Text>Tydzień</Text>
-        <Text>Miesiąc</Text>
+        <Pressable
+          onPress={() => {
+            setSelectedWeekCalendar(true);
+            setSelectedMonthCalendar(false);
+          }}
+          style={
+            [styles.selected,
+            selectedWeekCalendar && styles.selected_background]
+          }
+        >
+          <Text>Tydzień</Text>
+        </Pressable>
+        <Pressable
+          onPress={() => {
+            setSelectedWeekCalendar(false);
+            setSelectedMonthCalendar(true);
+          }}
+          style={
+            [styles.selected,
+            selectedMonthCalendar && styles.selected_background]
+          }
+        >
+          <Text>Miesiąc</Text>
+        </Pressable>
       </View>
     </View>
   );
@@ -18,15 +44,32 @@ const styles = StyleSheet.create({
   },
   toggle_container: {
     flexDirection: "row",
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    paddingHorizontal: 30,
+    justifyContent: "space-between",
+    alignItems: "center",
 
     width: "60%",
     height: 40,
-    backgroundColor: Colors.light_gray,
+    backgroundColor: "#f7f7f7",
 
     borderRadius: 20,
+  },
+  selected: {
+    width: "50%",
+    height: "95%",
+    borderRadius: 20,
+
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  selected_background: {
+    backgroundColor: Colors.green2,
+    width: "50%",
+    height: "95%",
+    borderRadius: 20,
+
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
 
